2161a87579380461a2f2477779804c1bc0d41ef6
Kiattikun <kiattikun.tho@ku.th>
2024-08-19 21:01:50 +0700

    [REMONVE] Remove theme-provider: remove theme-provider from components folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { Inter } from "next/font/google";
-import { ThemeProvider } from "@/components/theme-provider";
 import Link from "next/link";
 import client from "@/lib/pokemon-client";
 import { ApolloProvider } from '@apollo/client';
@@ -19,16 +18,14 @@ export default function RootLayout({
 		<ApolloProvider client={client}>
 			<html lang="en">
 				<body className={inter.className}>
-					<ThemeProvider attribute="class" defaultTheme="dark">
-						<main className="flex min-h-screen flex-col items-center p-24">
-							<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-								<Link href="/">
-									<h2 className="text-2xl text-bold">PokeDex</h2>
-								</Link>
-							</div>
-							{children}
-						</main>
-					</ThemeProvider>
+					<main className="flex min-h-screen flex-col items-center p-24">
+						<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+							<Link href="/">
+								<h2 className="text-2xl text-bold">PokeDex</h2>
+							</Link>
+						</div>
+						{children}
+					</main>
 				</body>
 			</html>
 		</ApolloProvider>
